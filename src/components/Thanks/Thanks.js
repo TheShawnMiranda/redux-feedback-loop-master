2a64dcff53312f9f7cc6551f2fef74503d32810e
Shawn Miranda <theshawnmiranda@gmail.com>
2020-02-29 22:10:45 -0600

    Base mode complete!

--- src/components/Thanks/Thanks.js
+++ src/components/Thanks/Thanks.js
@@ -1,17 +1,29 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 import { HashRouter as Router } from 'react-router-dom';
 import '../App/App.css';
 
 class Thanks extends Component {
+
+    formReset = () => {
+        this.props.reduxState.feedbackReducer = [];
+        this.props.history.push('/');
+    }
+
     render() {
         return (
             <Router>
                 <div className="Thanks">
                     <h1>Thank you for your Feedback!</h1>
+                    <button onClick={this.formReset}>Reset</button>
                 </div>
             </Router>
         )
     }
 }
 
-export default Thanks;
+const putReduxStateOnProps = (reduxState) => ({
+    reduxState
+})
+
+export default connect(putReduxStateOnProps)(Thanks);
